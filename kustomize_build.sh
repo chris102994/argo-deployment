6364bef977096221275a58bf21a2355fed456785
chris102994 <christopher102994@gmail.com>
2023-03-28 11:31:13 -0400

    Upgrading the deployment for bare-metal. Still a lot todo but this includes installing ingress and metallb.

--- kustomize_build.sh
+++ kustomize_build.sh
@@ -2,20 +2,26 @@
 
 set -e # Exit on error
 set -u # Unset variables are an error
+#set -o xtrace # Print commands to terminal
 
 APP_DIR="apps/"
 APP_DIRS=(`find "${APP_DIR}" -maxdepth 1 -mindepth 1 -type d`)
-HELM_LOCATION=$(which helm)
+HELM_LOCATION=$(which helm | true)
+KUBECTL_LOCATION=$(which kubectl)
 
-IGNORE_LIST=("argo-events", "argo-workflows")
+IGNORE_LIST=("argo-events", "argo-rollouts", "argo-workflows", "argocd", "metallb-load-balancer", "nginx-ingress")
 
 for APP in "${APP_DIRS[@]}"; do
   if [[ ! "${IGNORE_LIST[*]}" =~ "${APP//$APP_DIR}" ]]; then
     DEPLOY_FILE="${APP//$APP_DIR}_deployment.yaml"
     echo -e "${DEPLOY_FILE}"
-    kubectl kustomize "${APP}" -o "${DEPLOY_FILE}" --enable-helm --helm-command "${HELM_LOCATION}"
-    kubectl apply -f "${DEPLOY_FILE}"
+    if [ "${HELM_LOCATION:-UNDEFINED}" != "UNDEFINED" ]; then
+      "${KUBECTL_LOCATION}" kustomize "${APP}" -o "${DEPLOY_FILE}" --enable-helm --helm-command "${HELM_LOCATION}"
+    else
+      "${KUBECTL_LOCATION}" kustomize "${APP}" -o "${DEPLOY_FILE}"
+    fi
+    "${KUBECTL_LOCATION}" apply -f "${DEPLOY_FILE}"
+  else
+    echo -e "Ignoring ${APP}"
   fi
 done
-
-kubectl apply -f secrets.yaml
